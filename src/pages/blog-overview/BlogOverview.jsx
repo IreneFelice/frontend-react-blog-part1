2b4f4538ad33c458d3016793b6cd87419ee26daf
IreneFelice <i.f.buijing@gmail.com>
2025-01-02 12:49:20 +0100

    opdr2-foutmelding

--- src/pages/blog-overview/BlogOverview.jsx
+++ src/pages/blog-overview/BlogOverview.jsx
@@ -7,31 +7,41 @@ import {useEffect, useState} from "react";
 
 function BlogOverview() {
     const [posts, setPosts] = useState([]);
-
-
+    const [error, toggleError] = useState(false);
 
     useEffect(() => {
         async function findPosts() {
+            // RESET
+            toggleError(false);
+
             try {
-                const response = await axios.get('http://localhost:3000/posts');
+                const response = await axios.get('http://localhost:300/posts');
                 console.log(response);
                 setPosts(response.data);
             } catch (e) {
                 console.error(e);
+                toggleError(true);
             }
         }
+
         findPosts();
     }, []);
 
     return (
         <>
-            <h1>Welkom hier</h1>
+            <h1>Blog Overview</h1>
+            {error === true ? (
+                <>
+                <p>Alles is in de soep gelopen.. Probeer het opnieuw</p>
+                <p><Link to={'/'}>Home</Link></p>
+                </>
+            ):
             <ul>{Object.keys(posts).length > 0 && posts.map((post) => (
                 <li key={post.id}>
                     <p><Link to={`/blog-post/${post.id}`}>{post.title}</Link> ({post.author})</p>
                     <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
                 </li>))}</ul>
-
+            }
         </>
     )
 }
